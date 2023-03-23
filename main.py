fa87395a9ac67106b5bc3a762cda6b23fe052cea
CometovArt <cometovart@ya.ru>
2023-03-23 12:45:15 +0300

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import time
 from datetime import datetime, timedelta
 
-# текст для проверки синхронизации 4
+# текст для проверки синхронизации 5
 
 # https://apscheduler.readthedocs.io/en/3.x/modules/schedulers/asyncio.html
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
